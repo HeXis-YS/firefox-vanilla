3f5a73d8b16f90e70725ee77e7124255cb1fa110
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-09-10 10:06:36 +0800

    Fix paths

--- paths.sh
+++ paths.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-readonly REPO_DIR="$(realpath "$0")"
-readonly WORK_DIR="$(realpath "$0")"
+readonly REPO_DIR="$(dirname "$(realpath "$0")")"
+readonly WORK_DIR="$(dirname "$(realpath "$0")")"
 if [[ "$(uname)" == "Linux" ]]; then
   export PATH="${WORK_DIR}/venv/bin:${PATH}"
 fi
