c659c6a86e5bc7553604320b0a9dafb383445233
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-09-10 21:11:18 +0800

    Update scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source "$(dirname "$0")/paths.sh"
 
+if [[ "$1" != "windows" && "$1" != "linux" && "$!" != "android" ]]; then
+    exit 1
+fi
+
 if [[ "$(uname)" == "Linux" ]]; then
   sudo apt update
   sudo apt install -y python3 python3-pip python3-venv gcc mold mercurial watchman sccache
@@ -18,4 +22,17 @@ cd "$WORK_DIR"
 hg clone --stream --noupdate --config format.generaldelta=true "https://hg.mozilla.org/releases/mozilla-esr128" "firefox"
 pushd firefox
 hg update --clean --config extensions.fsmonitor= "FIREFOX_128_2_0esr_RELEASE"
+
+install -v $REPO_DIR/mozconfig-$1 $WORK_DIR/firefox/mozconfig
+
+case "$1" in
+  windows|linux)
+    hg clone --stream --noupdate --config format.generaldelta=true --config extensions.fsmonitor=  "https://hg.mozilla.org/l10n-central/zh-CN" "firefox-l10n"
+    python mach --no-interactive bootstrap --application-choice browser
+    ;;
+  android)
+    python mach --no-interactive bootstrap --application-choice mobile_android
+    ;;
+esac
+
 popd
--- build.sh
+++ build.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 source "$(dirname "$0")/paths.sh"
 
+if [[ "$1" != "windows" && "$1" != "linux" && "$!" != "android" ]]; then
+    exit 1
+fi
+
+pushd $WORK_DIR/firefox
+
+python mach configure
+python mach build
+
 case "$1" in
-    windows|linux)
-        install -v $REPO_DIR/mozconfig-$1 $WORK_DIR/firefox/mozconfig
-        pushd $WORK_DIR/firefox
-        python mach --no-interactive bootstrap --application-choice browser
-        popd
-        ;;
-    android)
-        install -v $REPO_DIR/mozconfig-android $WORK_DIR/firefox/mozconfig
-        pushd $WORK_DIR/firefox
-        python mach --no-interactive bootstrap --application-choice mobile_android
-        popd
-        ;;
-    *)
-        exit 1
+  windows|linux)
+    python mach package
+    python mach build installers-zh-CN
+    ;;
+  android)
+    export ANDROID_HOME=$(realpath ~/.mozbuild/android-sdk-linux)
+    export JAVA_HOME=$(realpath ~/.mozbuild/jdk/jdk-*)
+    pushd mobile/android/android-components
+    ./gradlew publishToMavenLocal
+    popd
+    pushd mobile/android/fenix
+    ./gradlew assembleRelease
+    popd
+    ;;
+  *)
+    exit 1
     ;;
 esac
 
-
-
-pushd $WORK_DIR/firefox
-python mach configure
-python mach build
 popd
