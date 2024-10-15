aaf530fa8d2baab6c30e22f77b89442557ce61c0
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2024-10-15 11:31:41 +0800

    Overhaul

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source "$(dirname "$0")/paths.sh"
 
-readonly BUILD_TAG="FIREFOX_128_2_0esr_RELEASE"
+readonly BUILD_TAG="FIREFOX_128_3_1esr_RELEASE"
 
-if [[ "$1" != "windows" && "$1" != "linux" && "$!" != "android" ]]; then
+if [[ "$1" != "windows" && "$1" != "android" ]]; then
     exit 1
 fi
 
 if [[ "$(uname)" == "Linux" ]]; then
   sudo apt update
-  sudo apt install -y python3 python3-pip python3-venv gcc mold mercurial watchman sccache
+  sudo apt install -y python3 python3-pip python3-venv gcc watchman sccache git
   python3 -m venv "${WORK_DIR}/venv"
   export CFLAGS="-march=native -mtune=native -Ofast -flto -flto-partition=none -fuse-linker-plugin -fgraphite-identity -floop-nest-optimize -fipa-pta -fno-semantic-interposition -fno-common -fdevirtualize-at-ltrans -fno-plt"
   export CXXFLAGS="${CFLAGS}"
@@ -21,21 +21,39 @@ if [[ "$(uname)" == "Linux" ]]; then
 fi
 
 cd "${WORK_DIR}"
-hg clone --stream --noupdate --config format.generaldelta=true "https://hg.mozilla.org/releases/mozilla-esr128" "firefox"
+hg clone --stream --noupdate --config format.generaldelta=true "https://hg.mozilla.org/releases/mozilla-esr128" firefox
 pushd firefox
 hg update --clean --config extensions.fsmonitor= "${BUILD_TAG}"
 watchman shutdown-server
-patch -p1 -N < "${PATCHES_DIR}/${BUILD_TAG}.diff"
+patch -p1 -N < "${PATCHES_DIR}/pgo.patch"
 install -v "${MOZCONFIGS_DIR}/$1" "${WORK_DIR}/firefox/mozconfig"
 
 case "$1" in
-  windows|linux)
+  windows)
     python mach --no-interactive bootstrap --application-choice browser
     hg clone --stream --config format.generaldelta=true --config extensions.fsmonitor=  "https://hg.mozilla.org/l10n-central/zh-CN" "${MOZBUILD_DIR}/l10n-central/zh-CN)"
     watchman shutdown-server
     ;;
   android)
-    python mach --no-interactive bootstrap --application-choice mobile_android
+    pushd mobile/android/fenix
+    sed -i \
+        -e 's|applicationId "org.mozilla"|applicationId "org.hexis"|' \
+        -e "s/Config.releaseVersionName(project)/\"128.3.1 ESR\"/" \
+        -e 's|"sharedUserId": "org.mozilla.firefox.sharedID"|"sharedUserId": "org.hexis.firefox.sharedID"|' \
+        -e '/CRASH_REPORTING/s/true/false/' \
+        -e '/TELEMETRY/s/true/false/' \
+        app/build.gradle
+    sed -i \
+        -e '/android:targetPackage/s/org.mozilla.firefox/org.hexis.firefox/' \
+        app/src/release/res/xml/shortcuts.xml
+    sed -i \
+        -e 's/aboutConfigEnabled(.*)/aboutConfigEnabled(true)/' \
+        app/src/*/java/org/mozilla/fenix/*/GeckoProvider.kt
+    sed -i -e "s/include \".*\"/include \"arm64-v8a\"/" app/build.gradle
+    popd
+    yes N | python mach --no-interactive bootstrap --application-choice mobile_android
+    export JAVA_HOME=$(realpath ${MOZBUILD_DIR}/jdk/jdk-*)
+    python mach python python/mozboot/mozboot/android.py --avd-manifest="python/mozboot/mozboot/android-avds/android31-x86_64.json" --no-interactive
     ;;
 esac
 
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source "$(dirname "$0")/paths.sh"
 
-if [[ "$1" != "windows" && "$1" != "linux" && "$!" != "android" ]]; then
+if [[ "$1" != "windows" && "$1" != "android" ]]; then
     exit 1
 fi
 
@@ -12,16 +12,23 @@ python mach build
 ~/.mozbuild/sccache/sccache --stop-server
 
 case "$1" in
-  windows|linux)
+  windows)
     python mach package
+    sed -i -e 's/ac_add_options MOZ_PGO=1/#ac_add_options MOZ_PGO=1/g' mozconfig
     python mach build installers-zh-CN
+    cp -v obj-x86_64-pc-windows-msvc/jarlog/en-US.log "${WORK_DIR}/"
+    cp -v obj-x86_64-pc-windows-msvc/instrumented/merged.profdata "${WORK_DIR}/"
+    cp -v "obj-x86_64-pc-windows-msvc/dist/install/sea/*.exe" "${WORK_DIR}/"
     ;;
   android)
-    export ANDROID_HOME=$(realpath ~/.mozbuild/android-sdk-linux)
-    export JAVA_HOME=$(realpath ~/.mozbuild/jdk/jdk-*)
-    pushd mobile/android/android-components
-    ./gradlew publishToMavenLocal
-    popd
+    export ANDROID_HOME=$(realpath ${MOZBUILD_DIR}/android-sdk-linux)
+    export JAVA_HOME=$(realpath ${MOZBUILD_DIR}/jdk/jdk-*)
+    export GECKO_PATH=$(realpath .)
+    python mach python testing/mozharness/scripts/android_emulator_pgo_local.py \
+      --config-file testing/mozharness/configs/android/android_common.py \
+      --config-file testing/mozharness/configs/android/android-aarch64-profile-generation_local.py \
+      --config-file testing/mozharness/configs/android/android_pgo.py \
+      --installer-path obj-aarch64-unknown-linux-android/gradle/build/mobile/android/test_runner/outputs/apk/withGeckoBinaries/debug/test_runner-withGeckoBinaries-debug.apk
     pushd mobile/android/fenix
     ./gradlew assembleRelease
     popd
