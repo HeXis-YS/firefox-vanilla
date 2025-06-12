d53e062cd57e48e181862994798f41c46103bb9f
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-06-12 23:11:45 +0800

    Re-enable some cipher suites for more compatibility

--- user.js
+++ user.js
@@ -91,16 +91,16 @@ pref("browser.translations.panelShown", false);
 pref("network.http.http3.enable", false);
 
 // Disable insecure and inefficient cipher suites
-pref("security.ssl3.ecdhe_ecdsa_aes_128_sha", false);
+// pref("security.ssl3.ecdhe_ecdsa_aes_128_sha", false);
 pref("security.ssl3.ecdhe_ecdsa_aes_256_gcm_sha384", false);
 pref("security.ssl3.ecdhe_ecdsa_aes_256_sha", false);
 pref("security.ssl3.ecdhe_ecdsa_chacha20_poly1305_sha256", false);
-pref("security.ssl3.ecdhe_rsa_aes_128_sha", false);
+// pref("security.ssl3.ecdhe_rsa_aes_128_sha", false);
 pref("security.ssl3.ecdhe_rsa_aes_256_gcm_sha384", false);
 pref("security.ssl3.ecdhe_rsa_aes_256_sha", false);
 pref("security.ssl3.ecdhe_rsa_chacha20_poly1305_sha256", false);
-pref("security.ssl3.rsa_aes_128_gcm_sha256", false);
-pref("security.ssl3.rsa_aes_128_sha", false);
+// pref("security.ssl3.rsa_aes_128_gcm_sha256", false);
+// pref("security.ssl3.rsa_aes_128_sha", false);
 pref("security.ssl3.rsa_aes_256_gcm_sha384", false);
 pref("security.ssl3.rsa_aes_256_sha", false);
 pref("security.tls13.aes_256_gcm_sha384", false);
